3e0ad8f0ffee134a4d3ffdecd196c9c214d15770
gaeon <rkdjs1104@ajou.ac.kr>
2024-06-22 21:48:26 +0900

    ✨feat: index.js - 한투 기간별 api 라우터 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const { returnDto } = require("../utils/DtoUtils");
 const handleCompanyNews =require( "../controllers/NewsCrawling");
 const {getCurrentPrice}=require( "../controllers/stockdetail/CurrentPrice");
 const {getDailyPrice}=require("../controllers/stockdetail/DailyPrice");
+const {getPeriodPrice}=require("../controllers/stockdetail/PeriodPrice");
 const fetchNews =require("../utils/NaverStockNews");
 
 /* GET home page. */
@@ -41,7 +42,6 @@ router.get('/daily-price', async (req, res) => {
   try {
   const symbol = req.query.symbol || '005930';
   const period=req.query.period 
-    // await auth();
     const price = await getDailyPrice(symbol,period ); // 현재 삼성전자로 하드코딩
     res.json(price);
   }
@@ -51,5 +51,20 @@ router.get('/daily-price', async (req, res) => {
   }
 });
 
+
+router.get('/period-price', async(req, res)=>{
+  try{
+    const symbol=req.query.symbol ||'005930';
+    const startday=req.query.startDate;
+    const endday=req.query.endDate;
+    const period=req.query.period;
+    const price=await getPeriodPrice(symbol, startday, endday, period);
+   
+    res.json(price.output2);
+  }catch (error) {
+    console.error('Error fetching data from external API:', error);
+    res.status(500).json({ error: 'Error fetching data from external API' });
+}
+})
 
 module.exports = router;
