2fe997f7349d07d85bd77b6797d1a801811c1822
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-06-26 09:20:05 +0900

    ✨ feat: 소셜 페이지 네이버 차트 api 구현 완료

--- src/routes/socialChart.js
+++ src/routes/socialChart.js
@@ -1,3 +1,4 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
 const googleTrends = require("google-trends-api");
@@ -48,6 +49,39 @@ router.get("/youtube", async (req, res) => {
   } catch (err) {
     console.error("에러 발생:", err);
     res.status(500).json({ error: "서버 에러가 발생했습니다." });
+  }
+});
+
+router.post("/naver", async (req, res) => {
+  const { keywords, startDate, endDate, periodOffset } = req.body;
+  const requestBody = {
+    startDate: startDate,
+    endDate: endDate,
+    timeUnit: Number(periodOffset) >= 365 ? "month" : "date",
+    keywordGroups: keywords.map((keyword) => ({
+      groupName: keyword,
+      keywords: [keyword],
+    })),
+  };
+  console.log("Request Body for Naver API:", requestBody);
+
+  try {
+    const response = await axios.post(
+      "https://openapi.naver.com/v1/datalab/search",
+      requestBody,
+      {
+        headers: {
+          "X-Naver-Client-Id": "MrI3E2Bf2lMdJjrb6HaJ",
+          "X-Naver-Client-Secret": "pAvfRnI4Lm",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log("result : " + response.data.results);
+    res.json(response.data.results);
+  } catch (error) {
+    console.error("Error fetching keyword data:", error);
+    res.status(500).send("Error fetching keyword data");
   }
 });
 
