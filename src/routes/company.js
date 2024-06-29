2f02fec68b12844f4fbbc8fc1a11510b75efdd7e
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-06-29 18:16:29 +0900

    ✨feat: company.js - AI 데이터 캐싱 적용

--- src/routes/company.js
+++ src/routes/company.js
@@ -3,11 +3,27 @@ var router = express.Router();
 const { returnDto } = require("../utils/DtoUtils");
 const { getSimilarityCompanies } = require("../controllers/Word2VecController");
 
+const cacheController = require("../controllers/CacheController");
+
 router.get("/", async function (req, res, next) {
   try {
     var searchWord = req.query.word;
-    var similarities = await getSimilarityCompanies(searchWord);
-    //var similarities = "333";
+    const keyword = searchWord;
+    const SOCIAL = "ai";
+    const PERIOD = 0;
+    let cache = await cacheController.getCache(keyword, SOCIAL, PERIOD);
+
+    if (cache === null || cache === undefined) {
+      const data = JSON.stringify(await getSimilarityCompanies(searchWord));
+      await cacheController.setCache(keyword, SOCIAL, PERIOD, data);
+      cache = await cacheController.getCache(keyword, SOCIAL, PERIOD);
+    }
+    if (cacheController.isExpired(cache)) {
+      const data = JSON.stringify(await getSimilarityCompanies(searchWord));
+      await cacheController.updateCache(keyword, SOCIAL, PERIOD, data);
+      cache = await cacheController.getCache(keyword, SOCIAL, PERIOD);
+    }
+    var similarities = JSON.parse(cache.dataValues.data);
     res.status(200).json(returnDto("A003", 200, similarities));
   } catch (error) {
     res.status(400).json(returnDto("E003", 400, "Error"));
